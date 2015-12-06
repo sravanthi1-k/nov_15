54463571d83f6342696ae543a442582ccc7106ab
venkatgn <venkatadrin.g@gmail.com>
2015-12-06 10:32:20 +0530

    first time

---
+++ MyMvnProj/src/test/java/yahoo/Test_prog.java
@@ -0,0 +1,45 @@
+package yahoo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import atu.testng.reports.listeners.ATUReportsListener;
+import atu.testng.reports.listeners.ConfigurationListener;
+import atu.testng.reports.listeners.MethodListener;
+
+@Listeners({ ATUReportsListener.class, ConfigurationListener.class,MethodListener.class })
+public class Test_prog 
+{
+
+	WebDriver driver;
+	 {
+		 System.setProperty("atu.reporter.config", "e:\\oct2015_sel\\myproj\\atu.properties");
+	 }
+	 
+	@Test
+	@Parameters({"browser"})
+	public void testing(String br) throws Exception
+	{
+		if(br.matches("firefox"))
+		{
+		  driver=new FirefoxDriver();
+		}
+		if(br.matches("chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver","c:\\chromedriver.exe");
+			driver=new ChromeDriver();
+		}
+		
+		Home h=new Home(driver);
+		h.createaccount();
+		h.login();
+		
+		Compose c=new Compose(driver);
+		c.sendmail();
+		c.signout();
+	}
+}
